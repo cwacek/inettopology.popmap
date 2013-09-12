2993bbfd4c85f41636c07bfd127c447ff0626662
Chris Wacek <cwacek@gmail.com>
2013-09-12 10:35:06 -0400

    Added initial structure, README, and package file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name="inettopology-popmap",
+    version="0.1",
+    packages=find_packages(),
+
+    install_requires=[
+        "redis",
+        "argparse",
+    ],
+
+    entry_points={
+        'inettopology.modules': [
+            'popmap = inettopology.popmap.cmdline'
+        ],
+    },
+
+    author="Chris Wacek",
+    author_email="cwacek@cs.georgetown.edu",
+    description="Internet Topology Graph Creator, Point-of-Presence Module",
+    license="LGPL"
+)
