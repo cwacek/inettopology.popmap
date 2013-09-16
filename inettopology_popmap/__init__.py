bec97351bc25b4326121593a45a72d7831405973
Chris Wacek <cwacek@gmail.com>
2013-09-16 18:35:48 -0400

    lazy_load now supports checking args afterwards

--- inettopology_popmap/__init__.py
+++ inettopology_popmap/__init__.py
@@ -1,12 +1,17 @@
 import importlib
 
 
-def lazy_load(package, function):
+def lazy_load(package, function, check_args=None):
   """ A helper to lazy load functions that start
-  actions to avoid circular references.
+  actions to avoid circular references. If :check_args:
+  is given, run that to allow checking arguments
+  specific to certain functions.
   """
   def runner(args):
+
+    if check_args is not None:
+      check_args(args)
+
     module = importlib.import_module("{0}.{1}".format(
                                      __name__, package))
     module.__dict__[function](args)
-
