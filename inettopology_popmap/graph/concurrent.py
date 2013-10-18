07bdcd83837a2921144f3359bb5b6781e67a0323
Chris Wacek <cwacek@gmail.com>
2013-10-18 14:26:13 -0400

    Added functionality for graph building

---
+++ inettopology_popmap/graph/concurrent.py
@@ -0,0 +1,235 @@
+import logging
+log = logging.getLogger(__name__)
+
+import sys
+import pqueue
+import os
+import networkx as nx
+
+import inettopology_popmap.connection as connection
+import inettopology_popmap.data.dbkeys as dbkeys
+from inettopology.util.general import Color, pairwise
+
+
+class ValleyFreeError(Exception):
+  pass
+
+
+def redirect_output():
+  """ Open a file and redirect all output to it """
+  log_out = open('pool_%s.log' % os.getpid(), 'w')
+  sys.stderr = log_out
+  sys.stdout = log_out
+
+
+def thread_shortest_path(graphpath, sp_key, type_key,
+                         node_key, path_key, used_key):
+
+    global thread_graph
+
+    log_out = redirect_output()
+
+    log_out.write("Spawned with sp_key = {0}; type_key = {1}; "
+                  "node_key={2}; path_key={3}\n"
+                  .format(sp_key, type_key, used_key, path_key))
+    log_out.flush()
+
+    r = connection.Redis()
+
+    log_out.write("Reading graph...")
+    log_out.flush()
+
+    thread_graph = nx.read_graphml(graphpath)
+
+    log_out.write(" Complete\n")
+    log_out.flush()
+
+    target = r.spop(sp_key)
+    while target:
+        used_nodes = set()
+        used_paths = set()
+        log_out.write("Obtaining shortest paths for %s... " % target)
+        log_out.flush()
+
+        paths = nx.single_source_dijkstra_path(
+            thread_graph, target, weight='latency')
+
+        for path_target, path in paths.iteritems():
+          if r.hget(type_key, path_target) in ('relay', 'client', 'dest'):
+            # We only care for relays, clients and destinations
+            try:
+              errors, total = check_valley_free(
+                  thread_graph, path, log=log_out)
+
+              if total > 0:
+                log_out.write("Path from %{0} to %s was valley-free. "
+                              " %0.0f/%0.0f (%0.2f) missing links in calc\n"
+                              % (target, path_target,
+                                 errors, total, errors / total))
+
+            except ValleyFreeError:
+              # If it's not valley free, we rebuild a new path
+              log_out.write("Path from %s to %s is not valley-free. "
+                            "Building a new one... "
+                            % (target, path_target))
+
+              try:
+                path, time_taken = valley_free_path(
+                    thread_graph, target, path_target)
+
+                log_out.write(" finished in %s seconds. New Path: %s "
+                              "[%0.0f/%0.0f (%0.2f%%) of links had no "
+                              "information]\n"
+                              % (Color.wrap(time_taken, Color.OKBLUE),
+                                 str(path), path.errct, len(path) - 1,
+                                 float(path.errct) / float(len(path) - 1)))
+
+              except ValleyFreeError:
+                log_out.write(
+                    Color.warn(
+                        "Couldn't produce valley-free path from %s to %s\n"
+                        % (target, path_target)))
+                raise
+
+            else:
+              # Now store the links and nodes from this
+              # path so that we keep them.
+              used_nodes.update(path)
+              hops = list()
+              for hop in pairwise(path):
+                  hops.append(hop)
+                  #Add each hop to our set of paths
+                  used_paths.add(hop)
+
+        r.sadd(path_key, *used_paths)
+        r.sadd(used_key, *used_nodes)
+        log_out.write("Done\n")
+        log_out.flush()
+        target = r.spop(sp_key)
+
+    log_out.write("Exiting ")
+    log_out.flush()
+    log_out.close()
+    sys.exit(0)
+
+
+def check_valley_free(g, path, log=None):
+  r = connection.Redis()
+
+  if r.get(dbkeys.AS.status('peering_data')) != "True":
+    sys.stderr.write("Attempted to check valley-free property, "
+                     "but have no peering data.\n")
+    return (0, 1)  # IF we have no peering data. It's irrelevant.
+
+  if len(path) == 0:
+    return (0, 1)
+
+  went_down = False
+  errct = 0.0
+  hopct = 0.0
+  asn_path = [g.node[hop]['asn']
+              for hop in path
+              if g.node[hop]['asn'] != "N/A"]
+
+  for as1, as2 in pairwise(asn_path):
+
+    hopct += 1
+    if as1 == as2:
+      continue
+
+    relationship = r().hget(dbkeys.AS.relationship(as1), as2)
+
+    if not relationship:
+      # Let's try the other side.
+      relationship = r().hget(dbkeys.AS.relationship(as2), as1)
+      if not relationship:
+        if log:
+          log.write("No relationship for %s <-> %s\n" % (as1, as2))
+        errct += 1
+        continue
+      else:
+        # We need to swap -1 and 1 for this side
+        # since it's the opposite perspective.
+        # If we swap a 2 to a -2, who cares.
+        relationship *= -1
+
+    if relationship == 1:
+      went_down = True
+
+    elif relationship == -1:  # AS1 is a customer of AS2
+      if went_down:
+        raise ValleyFreeError()
+
+  return (errct, hopct)
+
+
+class vfp(list):
+  def __init__(self):
+    list.__init__(self)
+    self.entered_valley = False
+    self.errct = 0
+
+
+def valley_free_path(g, start, path_target):
+  """
+  This is effectively a modified breadth first search implementation
+  which searches available paths, and truncates them when they violate
+  the valley-free property.
+  """
+  r = connection.Redis()
+
+  q = pqueue.pqueue()
+  q.push(vfp([start]), 0)
+
+  paths_found = pqueue.pqueue()
+
+  while not q.empty():
+    try:
+      # Get the lowest distance path.
+      dist, path = q.pop()
+    except IndexError:
+      break
+    else:
+      for linked_node in g[path[-1]]:
+        if linked_node not in path:  # Don't go backwards
+          newpath = path
+          newpath.append(linked_node)
+          as1 = g.node[path[-1]]['asn']
+          as2 = g.node[linked_node]['asn']
+
+          """ If this path enters a valley, fine. If it
+          enters a valley and tries to come back up, just remove it"""
+          relationship = r.hget(dbkeys.AS.relationship(as1), as2)
+          if not relationship:
+            # Let's try the other side.
+            relationship = r.hget(dbkeys.AS.relationship(as2), as1)
+            if not relationship:
+              newpath.errct += 1
+              continue
+            else:
+              # We need to swap -1 and 1 for this
+              # side since it's the opposite perspective.
+              # If we swap a 2 to a -2, who cares.
+              relationship *= -1
+
+          if relationship == -1:  # AS1 is a customer of AS2
+            if newpath.entered_valley:  # This path is no good
+              continue
+
+          elif relationship == 1:
+            newpath.entered_valley = True
+
+          if linked_node == path_target:
+            paths_found.push(newpath,
+                             dist + g[path[-1]][linked_node]['latency'])
+
+            if all(map(lambda x: x[0] > paths_found.peek(), q.queue)):
+              return paths_found.pop()
+
+          else:
+            q.push(newpath, dist + g[path[-1]][linked_node]['latency'])
+
+  try:
+    return paths_found.pop()
+  except IndexError:
+    raise ValleyFreeError()
---
+++ inettopology_popmap/graph/core.py
@@ -0,0 +1,558 @@
+import logging
+log = logging.getLogger(__name__)
+
+import sys
+import time
+import networkx as nx
+import operator
+import random
+import multiprocess
+
+from inettopology.util.decorators import timeit
+from inettopology.util.general import Color, ProgressTimer
+import inettopology_popmap.connection as connection
+import inettopology_popmap.data.dbkeys as dbkeys
+from inettopology_popmap.graph.objects import (
+    LinkDict, EdgeLink, VertexList, Stats)
+import inettopology_popmap.graph.objects as graph_objects
+import inettopology_popmap.graph.datautil as datautil
+import inettopology_popmap.graph.concurrent as concurrent
+from inettopology_popmap.data.cleanup import pipelined_delete
+
+
+def rand_key(keybase):
+    return "%s:%s" % (keybase, random.randint(0, 10000))
+
+
+SP_KEY = rand_key('core:sp_to_process')
+TYPE_KEY = rand_key('core:types')
+USED_KEY = rand_key('core:core_nodes')
+PATH_KEY = rand_key('core:core_paths')
+thread_graph = None
+thread_r = None
+log_out = None
+
+
+def create_graph(args):
+
+    r = connection.Redis()
+    if args.xml:
+
+        log.info("Loading saved graph from file: %s" % args.xml)
+
+        @timeit
+        def load_graph(path):
+          return nx.Graph(nx.read_graphml(path))
+
+        graph, time_taken = load_graph(args.xml)
+        log.info("Graph loading complete in %0.2f seconds" % time_taken)
+        graphpath = args.xml
+
+    else:
+        graph = load_from_redis(r, args)
+        graphpath = args.reload
+
+    used_nodes = set()
+
+    # used_nodes will contain only the vertices that we want in the graph.
+    # However, it contains all of the edges between those vertices, not
+    # only those used in paths.
+
+    log.info("Populating sources for shortest path... ")
+    nodetypes = nx.get_node_attributes(graph, 'nodetype')
+    p = r.pipeline()
+    p.delete(SP_KEY)
+    p.delete(USED_KEY)
+    for node, nodetype in nodetypes.iteritems():
+
+        if nodetype in ('relay', 'client', 'dest'):
+            p.sadd(SP_KEY, node)
+            p.hset(TYPE_KEY, node, nodetype)
+    p.execute()
+
+    log.info(Color.wrap("[complete]", Color.OKGREEN))
+
+    log.info("Spawning 2 workers to process shortest paths... ")
+
+    timer = ProgressTimer(int(r.scard(SP_KEY)))
+    left = timer.total
+    timer.tick(1)
+    #pool = Pool(processes=12, initializer=thread_init,
+                #initargs=(graphpath, SP_KEY, TYPE_KEY, USED_KEY, PATH_KEY, ))
+    workers = []
+    for x in xrange(2):
+        p = multiprocess.Process(
+            target=concurrent.thread_shortest_path,
+            args=(graphpath, SP_KEY, TYPE_KEY, USED_KEY, PATH_KEY, USED_KEY, ))
+
+        p.start()
+        workers.append(p)
+
+    while left > 0:
+        time.sleep(30)
+        timer.tick((timer.total - left) - timer.total_done)
+        sys.stderr.write(
+            "{0} Spawning workers to process shortest paths... {1}"
+            .format(Color.NEWL,
+                    Color.wrapformat("[ETA: {0}]", Color.OKBLUE, timer.eta())))
+
+        left = r.scard(SP_KEY)
+
+    log.info("\nWaiting for jobs to terminate... ")
+    for job in workers:
+        p.join()
+    #pool.join()
+    log.info("Done")
+
+    used_nodes = r.smembers(USED_KEY)
+    if len(used_nodes) == 0:
+      raise RuntimeError('Saved zero nodes after processing')
+    core_graph = graph.subgraph(used_nodes)
+    to_remove = list()
+
+    def rev_tuple(tup):
+        return (tup[1], tup[0])
+
+    for edge in core_graph.edges_iter:
+        if (not r.sismember(PATH_KEY, edge) and
+                not r.sismember(PATH_KEY, rev_tuple(edge))):
+            to_remove.append(edge)
+
+    log.info("Kept %s vertices in the graph" % len(used_nodes))
+    log.info("Removed %s extraneous edges" % len(to_remove))
+    core_graph.remove_edges_from(to_remove)
+
+    log.info("Re-trimming graph")
+    collapse_graph_in_place(graph)
+    log.info("Done")
+
+    log.info("Writing core graph... ")
+    nx.write_graphml(core_graph, "%s.xml" % args.save, prettyprint=True)
+
+    try:
+      nx.write_dot(core_graph, "%s.dot" % args.save)
+    except:
+      log.info("Failed to write dot graph")
+
+    log.info(Color.wrap("[complete]", Color.OKGREEN))
+
+    log.info("Cleaning up...")
+    pipelined_delete(r, TYPE_KEY, USED_KEY, PATH_KEY)
+
+
+def load_from_redis(r, args):
+    """
+    Create a igraph graph from redis
+    """
+
+    log.info("Loading from Redis")
+    linkdict = LinkDict(r)
+    vertices = VertexList()
+    tor_vertices = set()
+    graphlinks = []
+    graphattrs = dict()
+    graphattrs['latency'] = []
+    stats = Stats({'non-pop-trim': int,
+                   'unattachable-relays-count': int,
+                   'relay-latency-defaulted': int,
+                   'unattachable-relays': set,
+                   'num-relays': int,
+                   'num-pops': int,
+                   'num-links': int,
+                   'num-clients': int,
+                   'client-connect-points': int})
+
+    pipe = r.pipeline()
+    tor_asns = dict()
+    i = 0
+#Obtain the set of Tor relay IPs
+    if args.tor_relays:
+        #relays = dict()
+        log.info("Reading Tor relays from %s... " % args.tor_relays)
+        try:
+            with open(args.tor_relays) as f:
+                relay_ips = []
+                p = r.pipeline()
+                for line in f:
+                    ipline = line.split()
+                    ip = ipline[0]
+                    try:
+                        tor_asns[ip] = ipline[2]
+                    except:
+                        log.info("Couldn't add ASN for %s" % ip)
+                        raise Exception("Couldn't add ASN for %s" % ip)
+                    relay_ips.append(ip)
+                    p.hget('torip', ip)
+                relay_pops = p.execute()
+                p.reset()
+
+                relays = dict(zip(relay_ips, relay_pops))
+
+        except IOError as e:
+            log.info("Error: [%s]" % e)
+    else:
+        log.info("Reading all Tor relays from Redis...")
+        relays = r.hgetall("torip")
+
+    log.info(Color.wrap("Done", Color.OKBLUE))
+
+    log.info("Attaching clients to graph.")
+
+    #Add clients
+    if args.num_clients:
+
+        clients_attached, client_attach_points = add_asn_endpoints(
+            vertices,
+            graphlinks,
+            args.client_data,
+            args.num_clients,
+            'client')
+
+    log.info("Attached {0} clients to {1} attachment points".format(
+        clients_attached, client_attach_points))
+
+    log.info("Attaching destinations to graph.")
+    #Add dests
+    if args.num_dests:
+        dests_attached, dest_attach_points = add_asn_endpoints(
+            vertices,
+            graphlinks,
+            args.dest_data,
+            args.num_dests,
+            'dest')
+
+    log.info("Attached %s dests to %s attachment points".format(
+        dests_attached, dest_attach_points))
+
+    protected = set()
+    protected.update(relays.itervalues())
+    protected.update(vertices.keys())
+
+    # We want to trim all of the hanging edges of the graph.
+    log.info("Trimming degree-1 vertices...")
+    found_hanging_edge = True
+    pass_ctr = 0
+
+    while found_hanging_edge:
+        pass_ctr += 1
+        found_hanging_edge = False
+        removed = set()
+        n = 0
+        timer = ProgressTimer(len(linkdict))
+        for pop in linkdict.keys():
+
+            if n % 100 == 0 or n == timer.total - 1:
+                timer.tick(100)
+                sys.stderr.write(
+                    "{0}Pass {1}: {2} {3}".format(
+                        Color.NEWL,
+                        pass_ctr,
+                        Color.wrapformat("[{0} processed, {1} trimmed]",
+                                         Color.HEADER,
+                                         n, stats['non-pop-trim']),
+                        Color.wrapformat("[eta:{0}]",
+                                         Color.OKGREEN,
+                                         timer.eta())
+                    ))
+
+            n += 1
+
+            if pop in removed:
+                continue  # we saw this already
+            if len(linkdict[pop]) >= 2:
+                continue  # it can stay
+            if pop in protected:
+                continue  # We need relay/client/dest connect point
+
+            # It's only connected to one
+            connected = linkdict[pop].pop()
+            removed.add(pop)
+            del linkdict[pop]
+            linkdict[connected].remove(pop)
+            if len(linkdict[connected]) == 0:
+                # This was a matched pair attached to nothing else
+                del linkdict[connected]
+                removed.add(connected)
+            stats.incr('non-pop-trim')
+            found_hanging_edge = True
+        sys.stderr.write("\n")
+
+    linkdict.collapse_degree_two(protected=protected)
+
+    log.info("Trimmed {non-pop-trim} degree two hops".format(**stats))
+
+#Set vertex id's for all of the pops we have links for.
+    log.info("Adding PoPs...")
+    for pop in linkdict.iterkeys():
+        if pop in vertices:
+            continue  # we have this one already.
+
+        vertices.add_vertex(pop,
+                            nodeid=pop,
+                            nodetype='pop',
+                            asn=r.hget(dbkeys.POP.asn(pop)))
+
+        stats.incr('num-pops')
+        i += 1
+
+    log.info(Color.wrapformat("Added [{0}]", Color.OKBLUE, stats['num-pops']))
+
+    #Attach the relays
+    for relay_ip, conn_point in relays.iteritems():
+
+        if conn_point not in vertices:
+            stats.incr('unattachable-relays-count')
+            stats.incr('unattachable-relays', relay_ip)
+            continue
+
+        vertices.add_vertex(relay_ip, nodeid=relay_ip, nodetype='relay',
+                            asn=tor_asns[relay_ip])
+
+        linkdelays = sorted(
+            [delay
+             for edge in r.smembers(dbkeys.Link.intralink(conn_point))
+             for delay in r.smembers(dbkeys.delay_key(*eval(edge)))])
+
+        try:
+          deciles = decile_transform(linkdelays, default=5)
+        except graph_objects.EmptyListError:
+          deciles = [5 for x in xrange(10)]
+          stats.incr('relay-latency-defaulted')
+
+        graphlinks.append(EdgeLink(relay_ip, conn_point, {'latency': deciles}))
+
+        stats.incr('num-relays')
+        tor_vertices.add(relay_ip)
+        i += 1
+
+    log.info("Added {0} relays. Did not attach {1} "
+             "whose connection point was not linked to anything."
+             .format(stats['num-relays'],
+                     stats['unattachable-relays-count'])
+             )
+    log.info("Relays defaulted to 5ms links: [{0}]".format(
+        stats['relay-latency-defaulted']))
+
+    pipe.execute()
+
+    already_processed = set()
+    log.info("Processing links... ")
+    i = 0
+
+    for pop1 in linkdict.iterkeys():
+        if pop1 not in vertices:
+            continue
+
+        for pop2 in linkdict[pop1]:
+            if (pop2 not in vertices
+                    or dbkeys.Link.interlink(pop1, pop2) in already_processed):
+                continue
+
+            linkkey = dbkeys.Link.interlink(pop1, pop2)
+            linkdelays = sorted(
+                [delay
+                 for edge in r.smembers(linkkey)
+                 for delay in r.smembers(dbkeys.delay_key(*eval(edge)))])
+
+            try:
+              latency = decile_transform(linkdelays)
+            except graph_objects.EmptyListError:
+              latency = float(r.get("graph:collapsed:%s" %
+                                    (dbkeys.Link.interlink(pop1, pop2))))
+            graphlinks.append(EdgeLink(pop1, pop2, {'latency': latency}))
+
+            stats.incr('num-links')
+            already_processed.add(dbkeys.Link.interlink(pop1, pop2))
+
+        i += 1
+        sys.stderr.write("{0}Processed links for {1} pops"
+                         .format(Color.NEWL, i))
+
+    log.info("Processed {0} pop links "
+             .format(stats['num-links']))
+
+    log.info("Making Graph")
+    gr = nx.Graph()
+    gr.add_nodes_from(vertices.nx_tuple_iter)
+    gr.add_edges_from([edge.nx_tuple() for edge in graphlinks])
+
+    try:
+      bfs_edges = nx.bfs_edges(gr, linkdict.max_degree())
+    except:
+      print "Something was wrong with: %s" % linkdict.max_degree()
+      raise
+
+    bfs_node_gen = (node for pair in bfs_edges for node in pair)
+    subgraph = gr.subgraph(bfs_node_gen)
+
+    log.info("BFS reduced graph from %s to %s vertices".format(
+             len(gr), len(subgraph)))
+
+    log.info("Writing data file")
+    nx.write_graphml(gr, args.reload)
+
+    with open("vertices.dat", 'w') as vertout:
+        vertices.write(vertout)
+
+    log.info("Wrote files")
+
+    log.info("STATS:")
+    for key, val in stats.iteritems():
+      log.info("{0}: {1}".format(key, val))
+
+    return gr
+
+
+def add_asn_endpoints(vertex_list, linklist, datafile, count, endpointtype):
+    """
+    Add endpoint nodes that connect to the graph based on ASNs.
+
+    @param vertex_list: An instance of VertexList
+    @type vertex_list: C{VertexList}
+    @param linklist: A tuple containing a list of links, and an attribute
+        dictionary
+    @type linklist: C{tuple}
+    @param datafile: Path to a datafile that contains at least
+        two columns called 'Number' and 'ASN'. The 'Number'
+        column should indicate the relative number of endpoints
+        that should connect to that ASN.
+    @type datafile: C{str}
+
+    @param count: The number of endpoints to add.
+    @type count: C{int}
+    @param endpointtype: The label for the endpoint. 'client' is a
+        good example.
+    @type endpointtype: C{str}
+    """
+    r = connection.Redis()
+    try:
+        cdata_file = datautil.DataFile(datafile, sep="|")
+        cdata_file.add_index('ASN')
+    except IOError:
+        logging.error(Color.fail("Error reading %s" % datafile))
+        sys.exit(-1)
+
+    attach = dict()
+    for asn in cdata_file['ASN']:
+        try:
+            pop = find_pop_for_asn(asn)
+            attach[asn] = (pop, cdata_file['ASN'][asn][0]['Number'])
+        except graph_objects.ASNNotKnown:
+            pass
+
+    def node_id(asn, unique):
+        return "%s_%s_%s" % (endpointtype, asn, unique)
+
+    counter = 0
+    if len(attach) == 0:
+        sys.stderr.write(Color.fail(
+            "[failed] No %s could be attached.\n" % endpointtype))
+        logging.error(Color.fail(
+            "[failed] No %s could be attached." % endpointtype))
+
+    else:
+        total = sum(map(lambda x: int(x[1]), attach.itervalues()))
+        for asn, data in attach.iteritems():
+            num_to_attach = round(count * (float(data[1]) / float(total)))
+            for j in xrange(0, int(num_to_attach)):
+
+                vertex_list.add_vertex(node_id(asn, j), nodeid=node_id(asn, j),
+                                       nodetype=endpointtype, asn=asn)
+                linkkey = dbkeys.Link.intralink(data[0])
+
+                linkdelays = sorted(
+                    [delay
+                     for edge in r.smembers(linkkey)
+                     for delay in r.smembers(dbkeys.delay_key(*eval(edge)))])
+
+                try:
+                  latency = decile_transform(linkdelays)
+                except graph_objects.EmptyListError:
+                  latency = [5 for x in xrange(10)]
+
+                linklist.append(EdgeLink(node_id(asn, j),
+                                data[0], {'latency': latency}))
+                counter += 1
+
+        log.info(Color.wrapformat("Success [{0} attached]", Color.OKBLUE,
+                                  counter))
+    return (counter, len(attach))
+
+
+def collapse_graph_in_place(graph):
+    """ Collapse any degree two nodes in the graph *in-place*
+
+    Specifically, collapse degree two nodes where the ASN
+    of the node matches the ASN of the node on the other side.
+    """
+    asns = nx.get_node_attributes(graph, 'asn')
+    types = nx.get_node_attributes(graph, 'nodetype')
+    collapseable = True
+
+    ctr = 0
+    while collapseable:
+        ctr += 1
+        logging.info("Pass %s" % ctr)
+        to_collapse = []
+        zero_length = 0
+        exempt = set()
+        for node in nx.nodes_iter(graph):
+            if types[node] != 'pop':
+                continue
+
+            neighbors = graph.neighbors(node)
+            if len(neighbors) == 2:
+                # This link is collapsible if the asn matches that of
+                # one of its neighbors **and** none of the parties
+                # have already been collapsed on this pass
+                if ((asns[node] == asns[neighbors[0]]
+                   or asns[node] == asns[neighbors[1]])
+                   and len(exempt & (set([node]) | set(neighbors))) == 0):
+                    to_collapse.append(node)
+                    exempt |= set(neighbors)
+                    exempt |= set([node])
+
+        if len(to_collapse) != 0:
+            logging.info("Collapsing %s nodes" % len(to_collapse))
+
+            for node in to_collapse:
+                neighbors = graph.neighbors(node)
+                s1_weight = float(graph[node][neighbors[0]]['latency'])
+                s2_weight = float(graph[node][neighbors[1]]['latency'])
+                graph.add_edge(*neighbors, latency=s1_weight + s2_weight)
+                zero_length += 1 if s1_weight + s2_weight == 0 else 0
+                graph.remove_node(node)
+            logging.info("Created {0} new links. {1} with zero latency"
+                         .format(len(to_collapse), zero_length))
+        else:
+            collapseable = False
+
+
+def find_pop_for_asn(asn):
+  r = connection.Redis()
+
+  pops = r.smembers(dbkeys.ASN.pops(asn))
+
+  if len(pops) == 0:
+      raise graph_objects.ASNNotKnown("%s" % asn)
+
+  if len(pops) == 1:
+      return pops.pop()
+
+  popsize = [(r.scard(dbkeys.POP.members(pop)), pop)
+             for pop in pops]
+
+  return sorted(popsize, key=operator.itemgetter(0))[-1][1]
+
+
+def decile_transform(input_list, default=0):
+  sorted_list = sorted(input_list)
+
+  deciles = list()
+  interval = len(sorted_list) / 10
+
+  if len(sorted_list) == 0:
+    raise graph_objects.EmptyListError()
+  else:
+    for decile in xrange(10):
+      deciles[decile] = sorted_list[decile * interval]
